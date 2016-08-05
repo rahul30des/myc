aaa73d979c34faec08a75234e94ac8e944d54ea0
Rahul Deshpande <rahul30des@users.noreply.github.com>
2016-08-04 21:50:13 -0700

    2 Sum problem

---
+++ 2sum/2sum.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+
+using namespace std;
+
+// Main Function
+int main() {
+    unordered_map <int, int> hmap;
+    vector <int> arr = {1, 1, 2, 2, 4, 5, 3, 6, 6};
+    int target = 0;
+    cin>>target;
+/*
+// Time Complexity O(n)
+// Space Complexity O(n)
+    for(int i = 0; i < arr.size(); i++) {
+        int diff = target - arr[i];
+        if(hmap[arr[i]]) {
+            cout<<arr[i]<<" , "<<hmap[arr[i]]<<endl;
+            hamp.erase(arr[i]);
+        } else {
+            hmap[diff] = arr[i];
+        }
+    }
+*/
+    // Sort in O(nlogn) and then iterate from both sides in O(n)
+    // Time Complexity O(nlogn)
+    // Space Complexity O(1)
+    sort(arr.begin(), arr.end());
+    for(int i = 0, j = arr.size() - 1; i < j; ) {
+        int sum = arr[i] + arr[j];
+        if (sum == target) {
+            cout<<arr[i]<<" , "<<arr[j]<<endl;
+            i++;
+            j--;
+        } else if (sum < target) {
+            i++;    
+        } else if (sum > target) {
+            j--;
+        }
+    }
+    return 0;
+}
+
---
+++ 2sum/README.md
@@ -0,0 +1,8 @@
+2sum problem 
+
+Problem Statement : Accept an array of elements, target element.
+
+Output : 1. Return pair of elements that sum up to target,
+            do not print duplicate pairs for single pair 
+            of elements.
+         2. If there are n pairs in input, print n pairs in output
