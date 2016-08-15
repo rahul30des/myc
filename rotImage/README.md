56cd940405a73980a19e7145b7e52bcc305caa52
Rahul Deshpande <rahul30des@users.noreply.github.com>
2016-08-15 10:31:24 -0700

    Rotate image (square matrix) by 90 degrees
    Linear time when optimized.

---
+++ rotImage/README.md
@@ -0,0 +1,1 @@
+Given an image represented by N*N matrix can you rotate it 90 degrees in place.
---
+++ rotImage/rotImage.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+
+void rotate(vector<vector<int>>& matrix, int row, int col) {
+    for (int i = 0; i < row; i++) {
+        for(int j = i + 1; j < col; j++) {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+    for(int i = 0; i < (row/2); i++) {
+        for(int j = 0; j < col; j++) {
+            swap(matrix[i][j], matrix[col - i - 1][j]);
+        }
+    }    
+}
+
+
+void display(vector<vector<int>> matrix, int row, int col) {
+
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < col; j++) {
+            cout<<"\t"<<matrix[i][j];
+        }
+        cout<<endl;
+    }
+
+}
+
+// Main Function
+int main() {
+    vector <vector<int>> matrix = {{1,0},
+                                   {0, 1}};
+    int row = matrix.size();
+    int col = matrix[0].size();
+    rotate(matrix, row, col);
+    display(matrix, row, col);
+    return 0;
+}
