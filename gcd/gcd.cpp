2bf33f356d9b78077867d001d52fc7797d9b5585
Rahul Deshpande <rahul30des@gmail.com>
2021-08-08 14:19:16 -0700

    Calculate GCD of 2 numbers, uses coding guidelines https://google.github.io/styleguide/cppguide.html

--- gcd/gcd.cpp
+++ gcd/gcd.cpp
@@ -1,22 +1,22 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int euclid_gcd(int a, int b) {
-    while(b != 0) {
-        int r = a%b;
-        a = b;
-        b = r;
+int CalculateGCD(int first_num, int second_num) {
+    while(second_num != 0) {
+        int remainder = first_num % second_num;
+        first_num = second_num;
+        second_num = remainder;
     }
-    return a;
+    return first_num;
 }
 
+// Main Function
 int main() {
-    int a = 0, b = 0;
-    cin>>a>>b;
-    cout<<endl<<"GCD is : \t"<<euclid_gcd(a, b);
+    int first_num = 0, second_num = 0;
+    cin>>first_num>>second_num;
+    
+    cout<<endl<<"GCD is "<<CalculateGCD(first_num, second_num)<<endl;
     return 0;
 }
-
-
