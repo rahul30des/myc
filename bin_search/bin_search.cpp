88ff18199771e2af3ea15c789b451d2c3bb1775f
Rahul Deshpande <rahul30des@gmail.com>
2021-08-08 15:32:49 -0700

    Binary Search on a pre-sorted array

--- bin_search/bin_search.cpp
+++ bin_search/bin_search.cpp
@@ -1,28 +1,31 @@
 #include<iostream>
-#include<vector>
+#include<array>
 
 using namespace std;
 
-// Main Function
 int main() {
-    vector <int> arr;
-    int value;
-    cin>>value;
-    arr = {1, 4, 5, 6, 8};
-    int low = 0;
-    int high = arr.size() - 1;
-    while(low <= high) {
-        int mid = (low + high) / 2;
-        if (arr[mid] == value) {
-            cout<<"Value Found"<<endl;
-            return 0;
-        } else if (arr[mid] < value) {
-            low = mid + 1;
-        } else if (arr[mid] > value) {
-            high = mid - 1;
-        }
+    array<int,5> arr = {1, 3, 5, 7, 9};
+    // Assumption of a sorted array - Intro Sort costs O(nlogn) in the worst case and O(n) in the best case
+    // IntroSelect could also be assumed for O(n) worst case performance
+    
+    int search_term = -1;
+    cin>>search_term;
 
+    int low_idx = 0;
+    int high_idx = arr.size() - 1;
+
+    // Time Complexity O(log(n))
+    while(low_idx <= high_idx) {
+        int mid_idx = (low_idx + high_idx)/2;
+        if(arr[mid_idx] == search_term) {
+            cout<<endl<<"Search Term found"<<endl;
+            return 0;
+        } else if(arr[mid_idx] < search_term) {
+            low_idx = mid_idx + 1;
+        } else if(arr[mid_idx] > search_term) {
+            high_idx = mid_idx - 1;
+        }
     }
-    cout<<"Value Not Found"<<endl;
+    cout<<endl<<"Search Term not found"<<endl;
     return 0;
 }
