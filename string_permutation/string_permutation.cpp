ff5978fd01967073a4e4ef2da18cbb6f8d4137bd
Rahul Deshpande <rahul30des@gmail.com>
2021-08-14 05:45:39 -0700

    String Permutation in Exponential Time

--- string_permutation/string_permutation.cpp
+++ string_permutation/string_permutation.cpp
@@ -1,24 +1,23 @@
 #include<iostream>
-#include<string>
 
 using namespace std;
 
-// XOR swap 2 elements
+// Swap 2 charachters of a string using performant XOR bit manipulation
 void swap(char *str1, char *str2) {
     *str1 = *str1 ^ *str2;
     *str2 = *str1 ^ *str2;
     *str1 = *str1 ^ *str2;
 }
 
-// String Permutation - Time Complexity O(n * n!)
+// String Permutation - Time Complexit O(n * n!)
 void permute_string(string &str, int low, int high) {
-    if(low == high) {
+    if (low == high) {
         cout<<"\t"<<str;
     } else {
         for(int i = low; i <= high; i++) {
             swap(str[low], str[i]);
             permute_string(str, low + 1, high);
-            swap(str[i], str[low]);
+            swap(str[low], str[i]);
         }
     }
 }
@@ -27,7 +26,6 @@ void permute_string(string &str, int low, int high) {
 int main() {
     string str;
     cin>>str;
-
     permute_string(str, 0, str.size() - 1);
     return 0;
 }
