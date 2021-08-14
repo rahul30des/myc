af23e81065070db5240be4a10813ca79bd987fd7
Rahul Deshpande <rahul30des@gmail.com>
2021-08-14 06:11:59 -0700

    Rotate an image 90 degrees to the right

--- rotImage/rotImage.cpp
+++ rotImage/rotImage.cpp
@@ -1,42 +1,49 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-
-void rotate(vector<vector<int>>& matrix, int row, int col) {
-    for (int i = 0; i < row; i++) {
-        for(int j = i + 1; j < col; j++) {
-            swap(matrix[i][j], matrix[j][i]);
-        }
-    }
-    for(int i = 0; i < (row/2); i++) {
-        for(int j = 0; j < col; j++) {
-            swap(matrix[i][j], matrix[row - i - 1][j]);
-        }
-    }    
+// Swap 2 numbers using XOR bit manipulation operation
+void swap(int &num1, int &num2) {
+    num1 = num1 ^ num2;
+    num2 = num1 ^ num2;
+    num1 = num1 ^ num2;
 }
 
+// Rotate the image 90 degrees to the right
+void rotate(vector<vector<int>>& mat, int row, int col) {
+    // First Pass
+    for(int i = 0; i < row; i++) {
+        for(int j = i + 1; j < col; j++) {
+            swap(mat[i][j], mat[j][i]);
+        }
+    }
+    // Second Pass
+    for(int i = 0; i < row/2; i++) {
+        for(int j = 0; j < col; j++) {
+            swap(mat[i][j], mat[row - i - 1][j]);
+        }
+    }
+}
 
-void display(vector<vector<int>> matrix, int row, int col) {
-
+// Display all elements of a row
+void display(vector <vector<int>> mat, int row, int col) {
     for(int i = 0; i < row; i++) {
         for(int j = 0; j < col; j++) {
-            cout<<"\t"<<matrix[i][j];
+            cout<<"\t"<<mat[i][j];
         }
         cout<<endl;
     }
-
 }
 
 // Main Function
 int main() {
-    vector <vector<int>> matrix = {{1,0},
-                                   {0, 1}};
-    int row = matrix.size();
-    int col = matrix[0].size();
-    rotate(matrix, row, col);
-    display(matrix, row, col);
+    vector <vector<int>> mat (2, vector<int> (5));
+    mat = {{1, 0},
+           {0, 1}};
+    int row = mat.size();
+    int col = mat[0].size();
+    rotate(mat, row, col);
+    display(mat, row, col);
     return 0;
 }
